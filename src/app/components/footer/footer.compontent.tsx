eb8c09d59d3d5d665066db0e4cbadd5e2a3383f1
Luis Fernando <guffenix@gmail.com>
2024-07-07 10:39:29 -0500

    feat: footer component added

---
+++ src/app/components/footer/footer.compontent.tsx
@@ -0,0 +1,31 @@
+const navigation = {
+  main: [{ name: 'GitHub', href: 'https://github.com/wolfcito/mode-spray' }],
+}
+
+export function Footer() {
+  return (
+    <footer>
+      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+        <nav
+          aria-label="Footer"
+          className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
+        >
+          {navigation.main.map((item) => (
+            <div key={item.name} className="pb-6">
+              <a
+                href={item.href}
+                className="text-sm leading-6 text-gray-600 hover:text-gray-900"
+              >
+                {item.name}
+              </a>
+            </div>
+          ))}
+        </nav>
+
+        <p className="mt-10 text-center text-xs leading-5 text-gray-500">
+          &copy;2024 ModeSpray, Inc. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  )
+}
